3ea016e0750877e6851257cd16475b5e91c8fd3b
[dyad] <git@dyad.sh>
2025-07-18 21:36:21 +0200

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/PowerCrystal.tsx
+++ src/components/PowerCrystal.tsx
@@ -136,7 +136,7 @@ export const PowerCrystal: React.FC<PowerCrystalProps> = React.memo(({ onCrystal
       {/* Główny kontener dla kryształu i holdera - zmieniono na w-96 h-96 */}
       <div 
         className="relative w-96 h-96 flex items-center justify-center"
-        style={{ transform: 'translateY(25px)' }}
+        style={{ transform: 'translateY(28px)' }} {/* Zmieniono z 25px na 28px */}
       >
         {/* Holder Image - teraz w pełni w kontenerze */}
         <img
