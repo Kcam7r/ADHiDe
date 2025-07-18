a23f52407236380a5bf8a62d8158cb97d131722e
[dyad] <git@dyad.sh>
2025-07-18 13:06:59 +0200

    [dyad] Powiększono logo w pasku bocznym. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -34,7 +34,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ activeView, onViewChange }) =>
       <div className="w-64 bg-gray-800 h-screen flex flex-col">
         {/* Header with Logo and Title */}
         <div className="p-6 border-b border-gray-700 flex flex-col items-center justify-center">
-          <img src="/logo.png" alt="ADHiDe Logo" className="w-20 h-20 mb-2" /> {/* Zmieniono ścieżkę do logo */}
+          <img src="/logo.png" alt="ADHiDe Logo" className="w-40 h-40 mb-2" /> {/* Zmieniono rozmiar na w-40 h-40 */}
           <h1 className="text-2xl font-bold text-white text-center">ADHiDe</h1>
         </div>
 
