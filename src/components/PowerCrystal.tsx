7a7ade37f542ee6387387e63900da86151b20785
[dyad] <git@dyad.sh>
2025-07-18 18:41:41 +0200

    [dyad] Pomniejszono kryształ mocy i dostosowano jego pozycję. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/PowerCrystal.tsx
+++ src/components/PowerCrystal.tsx
@@ -21,9 +21,9 @@ export const PowerCrystal: React.FC<PowerCrystalProps> = React.memo(({ onCrystal
   // Persystowane właściwości stylu dla kryształu (top, left, size w px)
   // Wartości początkowe będą nadpisane przez te z localStorage, jeśli istnieją
   const [crystalProps, setCrystalProps] = useLocalStorage('adhd-crystal-props', {
-    top: 88, // Dostosowano do wyśrodkowania w kontenerze 320x320px dla rozmiaru 144px
-    left: 88, // Dostosowano do wyśrodkowania w kontenerze 320x320px dla rozmiaru 144px
-    size: 144, // Początkowy rozmiar (w px, w-36 to 144px)
+    top: 110, // Dostosowano do wyśrodkowania dla nowego rozmiaru
+    left: 110, // Dostosowano do wyśrodkowania dla nowego rozmiaru
+    size: 100, // Nowy, mniejszy rozmiar (w px)
   });
 
   const [crystalCenter, setCrystalCenter] = useState(() => {
