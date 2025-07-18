bbdacfecf00f5d4b222f50231e4c9cac4d2163a4
[dyad] <git@dyad.sh>
2025-07-18 22:55:08 +0200

    [dyad] Przesunięto Kryształ Mocy o 5px w dół. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/PowerCrystal.tsx
+++ src/components/PowerCrystal.tsx
@@ -19,7 +19,7 @@ export const PowerCrystal: React.FC<PowerCrystalProps> = React.memo(({ onCrystal
 
   // Stałe właściwości stylu dla kryształu, teraz kontrolowane przez rodzica
   const crystalSize = 100; // Rozmiar kryształu
-  const crystalTop = 107; // Pozycja Y wewnątrz holdera (zwiększona o 17px)
+  const crystalTop = 112; // Pozycja Y wewnątrz holdera (zwiększona o 5px)
   const crystalLeft = 62; // Pozycja X wewnątrz holdera
 
   const [crystalCenter, setCrystalCenter] = useState(() => {
