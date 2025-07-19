71dcea2bc6823c0e32804df4b0908441904a2a7d
[dyad] <git@dyad.sh>
2025-07-19 12:10:50 +0200

    [dyad] Przesunięto holder4.png o 60px w prawo i 40px w dół. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/PowerCrystal.tsx
+++ src/components/PowerCrystal.tsx
@@ -111,10 +111,10 @@ export const PowerCrystal: React.FC<PowerCrystalProps> = React.memo(({ onCrystal
           alt="Crystal Holder"
           className="absolute z-[6]" 
           style={{ 
-            top: -5, // 35 - 40 = -5
-            left: -68, // -28 - 40 = -68
-            width: 500, // 420 + 80 = 500
-            height: 'auto', // Zachowanie proporcji
+            top: 35, // Zmieniono z -5 na 35 (przesunięcie o 40px w dół)
+            left: -8, // Zmieniono z -68 na -8 (przesunięcie o 60px w prawo)
+            width: 500, 
+            height: 'auto', 
           }}
         />
 
