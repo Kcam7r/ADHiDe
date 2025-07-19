806d06cb069edb678e4495a02092a912022d8c6f
[dyad] <git@dyad.sh>
2025-07-19 12:51:51 +0200

    [dyad] Przesunięto kryształ mocy w prawo. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/PowerCrystal.tsx
+++ src/components/PowerCrystal.tsx
@@ -21,7 +21,7 @@ export const PowerCrystal: React.FC<PowerCrystalProps> = React.memo(({ onCrystal
   // Usunięto stałą containerHeight, aby wysokość była dynamiczna
 
   // Pozycje kryształu i podstawy dostosowane do nowego rozmiaru kontenera
-  const crystalSize = 112; // Zmieniono z 110 na 112, aby delikatnie powiększyć
+  const crystalSize = 112; 
   const xpForNextLevel = 1000;
   const xpInCurrentLevel = user.xp % xpForNextLevel;
   const xpProgress = xpInCurrentLevel / xpForNextLevel;
@@ -78,7 +78,7 @@ export const PowerCrystal: React.FC<PowerCrystalProps> = React.memo(({ onCrystal
   const crystalBottom = 93; 
 
   // Offset dla wyśrodkowania po zmianie rozmiaru
-  const horizontalOffset = 0.5; 
+  const horizontalOffset = 1.5; // Zmieniono z 0.5 na 1.5, aby przesunąć w prawo
 
   const auraIntensity = Math.min(1, dailyXpGain / 500);
   const auraColor = `rgba(255, 165, 0, ${auraIntensity * 0.8})`;
