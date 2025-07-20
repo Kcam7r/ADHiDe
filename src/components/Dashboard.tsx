f374533a88c0b224a2e026e1f731dbc700755582
[dyad] <git@dyad.sh>
2025-07-20 19:12:03 +0200

    [dyad] Dostosowano wysokość tabeli 'Nawyki' na pulpicie, ustawiając liczbę widocznych elementów na 9. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -330,7 +330,7 @@ export const Dashboard: React.FC = () => {
                 <span>✨</span>
                 <span>Nawyki</span>
               </h2>
-              <ScrollableList emptyMessage="Brak nawyków do wyświetlenia" itemHeightPx={60} visibleItemsCount={10}> {/* Zmieniono visibleItemsCount na 10 */}
+              <ScrollableList emptyMessage="Brak nawyków do wyświetlenia" itemHeightPx={60} visibleItemsCount={9}> {/* Zmieniono visibleItemsCount na 9 */}
                 {renderHabitItems()}
               </ScrollableList>
             </div>
