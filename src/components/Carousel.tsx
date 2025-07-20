8147332f702bd79833bb1eedec4a7cf4ea312e96
[dyad] <git@dyad.sh>
2025-07-20 16:26:38 +0200

    [dyad] wrote 2 file(s) + extra files edited outside of Dyad

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -64,7 +64,7 @@ export const Carousel: React.FC<CarouselProps> = ({
       onMouseDown={handleMouseDown}
       style={{ cursor: isDragging ? 'grabbing' : 'grab' }}
     >
-      <div ref={contentRef} className="flex-1 overflow-y-auto min-h-0">
+      <div ref={contentRef} className="flex-1 overflow-y-auto min-h-0 hide-scrollbar">
         <div className="space-y-3 flex-1 flex flex-col"> {/* Dodano flex-1 flex flex-col */}
           {items.length === 0 ? (
             <div className="text-gray-400 text-center flex-1 flex items-center justify-center">
