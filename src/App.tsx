af26d010f44198e642890b83c1ead396c97b0fde
[dyad] <git@dyad.sh>
2025-07-20 20:33:13 +0200

    [dyad] Poprawiono przewijanie i wyświetlanie strzałek w listach - wrote 2 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
           onOpenQuickThoughtsModal={() => setShowQuickThoughtsModal(true)}
           onOpenNewQuickThoughtModal={() => setShowQuickThoughtModal(true)} 
         />
-        <main className="flex-1 overflow-x-hidden overflow-y-hidden h-full"> {/* Zmieniono overflow-y-auto na overflow-y-hidden */}
+        <main className="flex-1 overflow-x-hidden h-full"> {/* Usunięto overflow-y-hidden */}
           <AnimatePresence mode="wait">
             <motion.div
               key={activeView}
