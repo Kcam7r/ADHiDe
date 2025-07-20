78e4b827c2d5b24a8c699b6f50c710ab513e9446
[dyad] <git@dyad.sh>
2025-07-20 16:40:46 +0200

    [dyad] Naprawiono przewijanie list na Pulpicie poprzez usunięcie zbędnej klasy wysokości z komponentu Carousel. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -60,12 +60,12 @@ export const Carousel: React.FC<CarouselProps> = ({
 
   return (
     <div 
-      className={`flex flex-col h-full ${className}`}
+      className={`flex flex-col ${className}`} // Usunięto h-full
       onMouseDown={handleMouseDown}
       style={{ cursor: isDragging ? 'grabbing' : 'grab' }}
     >
       <div ref={contentRef} className="flex-1 overflow-y-auto min-h-0 hide-scrollbar">
-        <div className="space-y-3 flex-1 flex flex-col"> {/* Dodano flex-1 flex flex-col */}
+        <div className="space-y-3 flex-1 flex flex-col">
           {items.length === 0 ? (
             <div className="text-gray-400 text-center flex-1 flex items-center justify-center">
               <p>Brak nawyków do wyświetlenia</p>
